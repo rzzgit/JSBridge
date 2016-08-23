2f3f58db650bce9b5df3b3e66d2c5c181fd0a1aa
rzz <rzzmail@qq.com>
2016-08-23 12:42:15 +0800

    封装成库，添加获取Activity

--- app/src/main/assets/JSBridge.js
+++ app/src/main/assets/JSBridge.js
@@ -2,6 +2,7 @@
  * Created by rzz on 2016/8/16.
  */
 var JSBridgeCallBacks = {};
+var TriggerNative;
 function getCallID(){
     return 'JSBridge-'+new Date().getTime()+'-xxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
@@ -21,5 +22,12 @@ function triggerNative(pluginName,method,data,success,error){
         }
         delete  JSBridgeCallBacks[callID];
     }
-    window.JSBridge.triggerNative(pluginName,method,JSON.stringify(data),callID);
+    if(TriggerNative){
+        TriggerNative(pluginName,method,JSON.stringify(data),callID);
+    }else{
+        error('init bridge error');
+    }
+    //window.JSBridge.triggerNative(pluginName,method,JSON.stringify(data),callID);
+    //window.webkit.messageHandlers.JSBridge.postMessage("aa","bbb");
 }
+
